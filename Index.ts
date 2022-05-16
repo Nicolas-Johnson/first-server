094df7d4a6feb03680c695c13a835dd1219770a8
Nicolas Johnson da SIlva Sousa <nicolas-sousa2011@hotmail.com>
2022-05-16 18:47:12 -0300

    All tables from id

--- Index.ts
+++ Index.ts
@@ -11,6 +11,27 @@ app.get("/", (req: Request, res: Response) => {
 
 });
 
+app.get("/accounts", async (req: Request, res: Response) => {
+
+  const connection = await db.connection();
+
+  const [ rows ] = await db.command("SELECT * FROM accounttypes;");
+  
+  res.json(rows);
+});
+
+app.get("/account/:id", async(req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (id) {
+    const connection = await db.connection();
+
+    const [ rows ] = await db.command(`SELECT * FROM accounttypes WHERE id = ?;`, [id]);
+
+    res.json(rows);
+  }
+});
+
 app.get("/customers", async (req: Request, res: Response) => {
 
   const connection = await db.connection();
@@ -30,6 +51,166 @@ app.get("/customer/:id", async (req: Request, res: Response) => {
   
     const [ rows ] = await db.command(`SELECT * FROM customer WHERE id = ?;`, [id]);
   
+    res.json(rows);
+  }
+
+});
+
+app.get("/agencies", async (req: Request, res: Response) => {
+
+  const connection = await db.connection();
+
+  const [ rows ] = await db.command("SELECT * FROM agencies;");
+
+  res.json(rows);
+});
+
+app.get("/agencie/:id", async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  if (id) {
+
+    const connection = await db.connection();
+
+    const [ rows ] = await db.command(`SELECT * FROM agencies WHERE id = ?;`, [id]);
+
+    res.json(rows);
+  }
+});
+
+app.get("/districts", async (req: Request, res: Response) => {
+
+  const connection = await db.connection();
+
+  const [ rows ] = await db.command("SELECT * FROM district;");
+
+  res.json(rows);
+});
+
+app.get("/district/:id", async (req: Request, res: Response) => {
+  const { id } = req.params;
+
+  if (id) {
+    const connection = await db.connection();
+
+    const [ rows ] = await db.command(`SELECT * FROM district WHERE id = ?;`, [id]);
+
+    res.json(rows);
+  }
+});
+
+app.get("/employees", async (req: Request, res: Response) => {
+
+  const connection = await db.connection();
+
+  const [ rows ] = await db.command("SELECT * FROM employees;");
+
+  res.json(rows);
+});
+
+app.get("/employee/:id", async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  if (id) {
+
+    const connection = await db.connection();
+
+    const [ rows ] = await db.command(`SELECT * FROM employees WHERE id = ?;`, [id]);
+
+    res.json(rows);
+  }
+});
+
+app.get("/managers", async (req: Request, res: Response) => {
+
+  const connection = await db.connection();
+
+  const [ rows ] = await db.command("SELECT * FROM managers;");
+
+  res.json(rows);
+
+});
+
+app.get("/manager/:id", async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  if (id) {
+
+    const connection = await db.connection();
+
+    const [ rows ] = await db.command(`SELECT * FROM managers WHERE id = ?;`, [id]);
+
+    res.json(rows);
+  }
+});
+
+app.get("/rules", async (req: Request, res: Response) => {
+
+  const connection = await db.connection();
+
+  const [ rows ] = await db.command("SELECT * FROM rules;");
+
+  res.json(rows);
+});
+
+app.get("/rule/:id", async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  if (id) {
+
+    const connection = await db.connection();
+
+    const [ rows ] = await db.command(`SELECT * FROM rules WHERE id = ?;`, [id]);
+
+    res.json(rows);
+  }
+});
+
+app.get("/services", async (req: Request, res: Response) => {
+
+  const connection = await db.connection();
+
+  const [ rows ] = await db.command("SELECT * FROM services;");
+
+  res.json(rows);
+});
+
+app.get("/service/:id", async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  if (id) {
+    const connection = await db.connection();
+
+    const [ rows ] = await db.command(`SELECT * FROM services WHERE id = ?;`, [id]);
+
+    res.json(rows);
+  }  
+});
+
+app.get("/timetable", async (req: Request, res: Response) => {
+
+  const connection = await db.connection();
+
+  const [ rows ] = await db.command("SELECT * FROM timetable;");
+
+  res.json(rows);
+});
+
+app.get("/timetable/:id", async (req: Request, res: Response) => {
+
+  const { id } = req.params;
+
+  if (id) {
+
+    const connection = await db.connection();
+
+    const [ rows ] = await db.command(`SELECT * FROM timetable WHERE id = ?;`, [id]);
+
     res.json(rows);
   }
 
